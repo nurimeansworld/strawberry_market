89ceb5804b2d23f67cd02c9cd1cd409674835c4d
nurimeansworld <nurimeansworld@gmail.com>
2022-01-09 22:52:21 +0900

    Add, Edit: 220109 로그인 페어프로그래밍(2)

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// L01
+const loginEmailInput = document.querySelector('.login-email-input');
+const loginPwInput = document.querySelector('.login-pw-input');
+const largeNextBtn = document.querySelector('.large-next-btn');
