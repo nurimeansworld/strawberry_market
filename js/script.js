b040c83caf4aab2a0ffb5d92da79e472d83a0be2
zeroto99 <myv_01@naver.com>
2022-01-14 17:59:12 +0900

    Add,Edit: 220114 로그인 페어프로그래밍(5)

--- js/script.js
+++ js/script.js
@@ -33,37 +33,44 @@ function getInput(){
 // 2. res에 있는 fetch 값 : 기본적으로 api 문서에 명세되어있는 내용을 보고 채우면 됨. method, headers, body 크게 3가지로 보통 명시되어 있음.
 // 3. body 값 형변환 : jSON.stringify를 하지 않으면 object 형태로 return 되기 때문에 우리가 처리하려는 string 형변환 해야 함.
 // 4. try{}catch(){} : 동작 수행 후 예외처리를 위해 선언. catch(err)로 err 내용 확인 가능. 정확한 원리는 추후 확인 필요.
-async function userLogin(e) {
+async function login(e) {
   e.preventDefault(); // button form 전송하지 않게 초기화
 
   const userEmail = loginEmailInput.value;
   const userPw = loginPwInput.value;
   const url = "http://146.56.183.55:5050";
-
-  console.log(userEmail);
-  console.log(userPw);
+  const loginData = {
+    "user": {
+      "email": userEmail,
+      "password": userPw
+    }
+  };
 
   try{
-    const res = await fetch( url+'/user/login', {
-        method: "POST",
-        headers: {
-          "Content-type" : "application/json"
-        },
-        body: JSON.stringify({
-          "user":{
-            "email": userEmail,
-            "password": userPw
-          }
-        })
+    const res = await fetch(url+'/user/login', {
+      method: "POST",
+      headers: {
+        "Content-type" : "application/json"
+      },
+      body: JSON.stringify(loginData),
     });
     const resJson = await res.json();
     console.log('resJson', resJson);
+
+    if(resJson.status == 200){ // 로그인 성공한 경우
+      localStorage.setItem("Token",resJson.user.token);
+      location.href = "./H01.html";
+    }else{ // 로그인 실패한 경우
+      // MEMO:: 나중에 다른 기능과 같이 함수화 할 기능이 생기면 함수화 하기 - setErrorMessage();
+      LoginError.style.display = 'block'; // LoginError.classList.add = 'on';
+      LoginError.textContent = resJson.message;
+    }
   }catch(err){
-    console.log('err', err);
+    // 아예 api와 연결이 되지 않았을 때
+    // console.log('err', err);
   } 
 }
-
-largeNextBtn.addEventListener('click', userLogin);
+largeNextBtn.addEventListener('click', login);
 
 
 // 누리가 작성한 찌끄래기 낙서...
@@ -73,22 +80,6 @@ largeNextBtn.addEventListener('click', userLogin);
 // 아이디&&비번 일치하지 않을 지 error 값 return
 // 이메일&비밀번호 입력 값 유효성 검사ㅏ(js)
 
-//(유효성 검사) 2. 로그인 버튼을 클릭하면 이메일 주소와 로그인에 대한 유효성 검사를 진행하며, 이메일 주소 또는 비밀번호가 일치하지 않을 경우에는 경고 문구가 나타납니다.
-LoginError.addEventListener('', function(){
-  if(!loginEmailInput.value == 'test@test.com'){
-    
-  }
-})
 
-// // 3. 로그인 버튼이 클릭되면 입력한 이메일과 비번이 데이터에 있는지 확인 
-// largeNextBtn.addEventListener('click', () => {
-//   if(loginEmailInput.value = 데이터에 있다면 && loginPwInput.value === 데이터에 있다면) {
-//     // -> true 홈 페이지로 이동 (로그인됨)
-//     location.href = 'H01.html';
-//   } else {
-//     // -> false 에러 메세지 띄우기 
-//     LoginError.style.display = 'block'
-//   }
-// })
 
 // 정규식 관련 참고 블로그 링크 : https://rateye.tistory.com/468
