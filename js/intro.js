f9b8143b63eda45a347992e0c34ac3a85e548a91
youngeun choi <choi46910@naver.com>
2022-02-24 18:11:37 +0900

    Add: 영은 intro.js 위치 변경

--- js/intro.js
+++ js/intro.js
@@ -1,7 +1,7 @@
 // 로그인하여 localStorage에 값이 있는 경우 해당 페이지 접근 불가
 function checkLogin() {
   if (localStorage.getItem("Token")) {
-    location.href = "./home.html";
+    location.href = "./followfeed.html";
   }
 }
 checkLogin();
